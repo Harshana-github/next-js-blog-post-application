ce52bbeb05b8917fb03c0c2239f4853b0e04f8bd
Harshana Lakmal <harshanalakmal503@gmail.com>
2022-12-30 10:29:40 +0530

    prepared for deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "brand1234",
+        mongodb_password: "1234",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: "brand1234",
+      mongodb_password: "1234",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  };
+};
